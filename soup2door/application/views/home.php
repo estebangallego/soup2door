0518ec01c88b44659c9cdc719e5e124960fcded0
estebangallego <egallego00@gmail.com>
2015-02-06 12:43:43 -0800

    starting_project

---
+++ soup2door/application/views/home.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4"><h2>Hello Word</h2></div>
+	</div>
+	<div class="row">
+		<div class="col-xs-6 col-md-3">.col-xs-6 .col-md-3</div>
+ 		<div class="col-xs-6 col-md-3">.col-xs-6 .col-md-3</div>
+ 		<div class="col-xs-6 col-md-3">.col-xs-6 .col-md-3</div>
+ 		<div class="col-xs-6 col-md-3">.col-xs-6 .col-md-4</div>
+	</div>
+</div>
