a4b8e35a218c6458e676054180b92777967654ae
brendan_stanton <bmsenate@gmail.com>
2015-02-07 16:37:10 -0800

    commiting views to branch

--- soup2door/application/controllers/Main.php
+++ soup2door/application/controllers/Main.php
@@ -6,13 +6,12 @@ class Main extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->output->enable_profiler();
 	}
 	public function index()
 	{
 		$data['title'] = 'Hello World';
-		$this->load->view('templates/header', $data);
-		$this->load->view('home');
+		$this->load->view('admin_header', $data);
+		$this->load->view('admin_dashboard');
 		$this->load->view('templates/footer');
 	}
 }
